bd364df9d89ae2ef0ce887557596846ff1686ed9
ChunggiLee <9670425@naver.com>
2016-05-06 23:46:51 +0900

    TapRPG ver1.4

---
+++ Assets/Scripts/Game/KillZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillZone : MonoBehaviour {
+
+    void OnTriggerStay2D(Collider2D _hit)
+    {
+        if (_hit.tag == "Monster")
+        {
+            _hit.GetComponent<MonsterFSM>().SetState(CharacterState.Dead);
+
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ItemDatabase.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour {
+
+    public List<Item> items = new List<Item>();
+
+    void Start()
+    {
+        items.Add(new Item("armor_002", 0, "Nice Armor", 10, 10, 1, Item.ItemType.Clothes));
+        items.Add(new Item("armor_009", 1, "Better Armor", 10, 10, 1, Item.ItemType.Clothes));
+        items.Add(new Item("potion_08", 2, "Nice Armor", 10, 10, 1, Item.ItemType.Consumable));
+        
+
+
+    }
+}
